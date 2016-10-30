788f809eff2757c4698e53e538189cb0cbf342a0
Peng Huan Hung <pernghh@gmail.com>
2016-10-30 10:13:26 +0800

    Add handler class.

---
+++ AudioDirection/AudioDirectionDetector.swift
@@ -0,0 +1,13 @@
+//
+//  AudioDirectionDetector.swift
+//  AudioDirection
+//
+//  Created by Peng Huan Hung on 2016/10/30.
+//  Copyright © 2016年 Peng Huan Hung. All rights reserved.
+//
+
+import UIKit
+
+class AudioDirectionDetector: NSObject {
+
+}
